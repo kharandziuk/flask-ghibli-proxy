6eeb5bd9ca95d06e659849b91f420a5cb2b2f6ee
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-07-10 14:11:37 +0200

    solution before cleanup

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
+import json
 
-from flask import Flask
 from celery import Celery
 from dotenv import load_dotenv
+from flask import Flask, redirect, url_for, render_template
 import redis
+import requests
 
 load_dotenv(verbose=True)
 
-CACHE_KEY_NAME = 'cache:films-template'
-redis_host = "localhost"
-redis_port = 6379
-redis_password = ""
+GHIBLI_API_URL = 'https://ghibliapi.herokuapp.com/films/'
+CACHE_KEY_NAME = 'cache:films'
 
 app = Flask(__name__)
 
@@ -19,12 +19,6 @@ app.config.update(
     CELERY_RESULT_BACKEND=os.getenv('CELERY_RESULT_BACKEND')
 )
 
-redis_client = redis.StrictRedis(
-        host=redis_host,
-        port=redis_port,
-        password=redis_password,
-        decode_responses=True
-        )
 
 def make_celery(app):
     celery = Celery(
@@ -41,19 +35,66 @@ def make_celery(app):
 
     celery.Task = ContextTask
     return celery
-celery = make_celery(app)
 
-time = ''
+celery = make_celery(app)
+redis_host = "localhost"
+redis_port = 6379
+redis_password = ""
+redis_client = redis.StrictRedis(
+        host=redis_host,
+        port=redis_port,
+        password=redis_password,
+        decode_responses=True
+        )
+
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(1.0, set_response.s(), name='set up current time')
+    sender.add_periodic_task(60.0, set_cache.s(), name='set up current time')
+
+
+def prepare_template():
+    
+    return 
+def get_people_data(people_urls, film_url):
+    # api behaves weird here: sometimes it returns a link for all the characters from all the movies
+    if people_urls == ['https://ghibliapi.herokuapp.com/people/']:
+        people_data = requests.get(people_urls[0]).json()
+        people_data = [entry for entry in people_data if film_url in entry['films']]
+        return [entry['name'] for entry in people_data]
+    else:
+        return [
+            requests.get(people_url).json()['name']
+            for people_url in people_urls
+        ]
+
+
+def get_films_data():
+    result = []
+    films = requests.get(GHIBLI_API_URL).json()
+    for film in films:
+        people = get_people_data(film['people'], film['url'])
+        result.append([film['title'], people])
+    return result
 
 @celery.task()
-def set_response():
-    import datetime
-    redis_client.set(CACHE_KEY_NAME, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+def set_cache():
+    films_data = get_films_data()
+    redis_client.set(
+        CACHE_KEY_NAME,
+        json.dumps(films_data)
+    )
+
+if not redis_client.exists(CACHE_KEY_NAME):
+    set_cache()
 
 @app.route('/')
-def hello_world():
-    response = redis_client.get(CACHE_KEY_NAME)
-    return response
+def root():
+    return redirect(url_for('movies'))
+
+@app.route('/movies', methods=['GET'])
+def movies():
+    films_json = redis_client.get(
+        CACHE_KEY_NAME,
+    )
+    films = json.loads(films_json)
+    return render_template('movies.html', films=films)
