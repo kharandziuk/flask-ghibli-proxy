ef91ad4255e3c1c38740351bf2dac5617208a053
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-07-10 11:26:25 +0200

    mvp

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import os
 from flask import Flask
 from celery import Celery
 from dotenv import load_dotenv
+import redis
 
 load_dotenv(verbose=True)
+
+CACHE_KEY_NAME = 'cache:films-template'
+redis_host = "localhost"
+redis_port = 6379
+redis_password = ""
 
 app = Flask(__name__)
 
@@ -12,6 +18,13 @@ app.config.update(
     CELERY_BROKER_URL=os.getenv('CELERY_BROKER_URL'),
     CELERY_RESULT_BACKEND=os.getenv('CELERY_RESULT_BACKEND')
 )
+
+redis_client = redis.StrictRedis(
+        host=redis_host,
+        port=redis_port,
+        password=redis_password,
+        decode_responses=True
+        )
 
 def make_celery(app):
     celery = Celery(
@@ -30,12 +43,17 @@ def make_celery(app):
     return celery
 celery = make_celery(app)
 
+time = ''
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(1.0, set_response.s(), name='set up current time')
+
 @celery.task()
-def add_together(a, b):
-    return a + b
+def set_response():
+    import datetime
+    redis_client.set(CACHE_KEY_NAME, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
 @app.route('/')
 def hello_world():
-    result = add_together.delay(23, 42)
-    r = result.get()
-    return str(r)
+    response = redis_client.get(CACHE_KEY_NAME)
+    return response
